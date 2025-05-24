e0b25b5c7b619516bc32e81693a92c2a9741d85b
jhoyosR <jhoyos0410@cue.edu.co>
2025-05-23 20:56:46 -0500

    New: Crea CRUD de usuarios

---
+++ usuarios-service/src/main/java/edu/humboldt/usuarios/User/UpdateUserRequest.java
@@ -0,0 +1,17 @@
+package edu.humboldt.usuarios.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+    private String username;
+    private String email;
+    private String password;
+    private String roleId;
+}
--- usuarios-service/src/main/java/edu/humboldt/usuarios/User/UserController.java
+++ usuarios-service/src/main/java/edu/humboldt/usuarios/User/UserController.java
@@ -1,38 +1,68 @@
-// package edu.humboldt.usuarios.User;
+package edu.humboldt.usuarios.User;
 
-// import java.util.List;
-// import java.util.Optional;
-// import org.springframework.web.bind.annotation.*;
-// import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// @RestController
-// @RequestMapping("/api/users")
-// public class UserController {
-//     @Autowired private UserService userService;
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
 
-//     @GetMapping
-//     public List<User> getAll() { 
-//         return userService.findAll(); 
-//     }
+    @Autowired 
+    private UserService userService;
+    @Autowired
+    private RoleService roleService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-//     @GetMapping("/{id}")
-//     public Optional<User> getById(@PathVariable String id) {
-//         return userService.findById(id);
-//     }
 
-//     @PostMapping
-//     public User create(@RequestBody User user) {
-//         return userService.save(user);
-//     }
+    @GetMapping
+    public List<User> getAll() { 
+        return userService.findAll(); 
+    }
 
-//     @PutMapping("/{id}")
-//     public User update(@PathVariable String id, @RequestBody User user) {
-//         user.setId(id);
-//         return userService.save(user);
-//     }
+    @GetMapping("/{id}")
+    public Optional<User> getById(@PathVariable String id) {
+        return userService.findById(id);
+    }
 
-//     @DeleteMapping("/{id}")
-//     public void delete(@PathVariable String id) {
-//         userService.deleteById(id);
-//     }
-// }
+    @PostMapping
+    public User create(@RequestBody CreateUserRequest request) {
+        Optional<Role> optionalRole = roleService.findById(request.getRoleId());
+        if (optionalRole.isEmpty()) {
+            return null;
+        }
+        Role role = optionalRole.get();
+
+        User user = new User(null, request.getUsername(), request.getEmail(), passwordEncoder.encode(request.getPassword()), role);
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable String id, @RequestBody UpdateUserRequest request) {
+        Optional<User> optionalUser = userService.findById(id);
+        if (optionalUser.isEmpty()) {
+            return null;
+        }
+        Optional<Role> optionalRole = roleService.findById(request.getRoleId());
+        if (optionalRole.isEmpty()) {
+            return null;
+        }
+
+        User user = optionalUser.get();
+        Role role = optionalRole.get();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(role);
+
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        userService.deleteById(id);
+    }
+}
