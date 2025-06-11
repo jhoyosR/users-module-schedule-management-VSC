0e1dd942558d07d62481a099945cc6600f54d75d
jhoyosR <jhoyos0410@cue.edu.co>
2025-06-11 17:27:46 -0500

    New: Crea endpoint específico para el inicio de sesión de los administradores

---
+++ usuarios-service/src/main/java/edu/humboldt/usuarios/Auth/AuthAdminService.java
@@ -0,0 +1,29 @@
+package edu.humboldt.usuarios.Auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import edu.humboldt.usuarios.Jwt.JwtService;
+import edu.humboldt.usuarios.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthAdminService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+            .token(token)
+            .build();
+
+    }
+}
